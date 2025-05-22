1e755a755797704246b15a73da803db100532df2
Jason Tsai <git@pews.dev>
2025-05-22 16:13:38 +0900

    ci: await report exported and extract the filename

--- scripts/test.js
+++ scripts/test.js
@@ -12,14 +12,8 @@ onload = () => {
 };
 
 async function onBcdTestComplete(results) {
-  await sendReport(results);
-
-  // Trigger report export.
-  // This will make collector generate a json report in the `collector/download/` directory.
-  let exportElement = document.getElementById('export');
-  exportElement.submit();
-
-  console.log('Export submitted');
+  await sendTestResults(results);
+  await exportReport();
 
   // Close the window to terminate the servo process.
   window.close();
@@ -30,8 +24,10 @@ function extractResourceCount(input) {
   return match ? parseInt(match[1], 10) : null;
 }
 
-async function sendReport(results) {
-  console.log('Sending report');
+// Send test results to collector.
+// Collector will store the results in its session storage temporarily.
+async function sendTestResults(results) {
+  console.log('Beginning sending report to collector...');
 
   const resultsURL =
     (location.origin || location.protocol + '//' + location.host) +
@@ -46,9 +42,31 @@ async function sendReport(results) {
     body: JSON.stringify(results),
   });
 
-  if (!response.ok) {
-    console.error('Failed to send report:', response.statusText);
-  } else {
+  if (response.ok) {
     console.log('Report sent successfully');
+  } else {
+    console.error('Failed to send report:', response.statusText);
+  }
+}
+
+// Send export request to collector.
+// This will make collector generate a json report in the `collector/download/` directory.
+async function exportReport() {
+  console.log('Beginning report export...');
+
+  const resultsURL =
+    (location.origin || location.protocol + '//' + location.host) + '/export';
+
+  const response = await fetch(resultsURL, {
+    method: 'POST',
+  });
+
+  if (response.ok) {
+    console.log('Report exported successfully');
+    let responseText = await response.text();
+    let filename = responseText.match(/<a.*href="\/download\/(.*)"/)[1];
+    console.log(`RESULT_FILENAME: ${filename}`);
+  } else {
+    console.error('Failed to export report:', response.statusText);
   }
 }
